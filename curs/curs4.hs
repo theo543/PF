dbbacf687bcb60c9281dc571e7323f18ea57bb99
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-26 12:56:04 +0300

    more 4

--- curs/curs4.hs
+++ curs/curs4.hs
@@ -1,7 +1,16 @@
-import Data.Char (ord)
+import Data.Char (ord, isDigit)
 
 squares :: [Int] -> [Int]
 squares = map (^ 2)
 
 ords :: [Char] -> [Int]
 ords = map ord
+
+positives :: [Int] -> [Int]
+positives = filter (> 0)
+
+digits :: [Char] -> [Char]
+digits = filter isDigit
+
+filteredfns = filter (\(name, fn) -> fn 5) [("> 10", (> 10)), ("> 2", (> 2)), ("> 4", (> 4)), ("is odd", odd), ("is even", even), ("is not 0", (/= 0))]
+showfns = map (\(name, fn) -> "5 " ++ name) filteredfns
