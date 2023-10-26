3866895ce48f52d8cae6c57b1fb48571fde00d69
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-26 22:24:57 +0300

    better filtered fns

--- curs/curs4.hs
+++ curs/curs4.hs
@@ -12,5 +12,11 @@ positives = filter (> 0)
 digits :: [Char] -> [Char]
 digits = filter isDigit
 
-filteredfns = filter (\(name, fn) -> fn 5) [("> 10", (> 10)), ("> 2", (> 2)), ("> 4", (> 4)), ("is odd", odd), ("is even", even), ("is not 0", (/= 0))]
-showfns = map (\(name, fn) -> "5 " ++ name) filteredfns
+data NamedPredicate a = NP {name :: String, predicate :: a -> Bool}
+
+instance Show (NamedPredicate a) where
+    show :: NamedPredicate a -> String
+    show p = "function named '" ++ name p ++ "'"
+
+namedFns :: [NamedPredicate Int]
+namedFns = filter (($ 5) . predicate) [NP "> 10" (> 10), NP "> 2" (> 2), NP "> 4" (> 4), NP "is odd" odd, NP "is even" even, NP "is not 0" (/= 0)]
