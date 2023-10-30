3269d4020cc6bb466f3b9b959ab33d8d4a4fcfb2
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-30 15:53:09 +0200

    add lab5 template

---
+++ lab/lab5.hs
@@ -0,0 +1,20 @@
+allVerifies :: (Int -> Bool) -> [Int] -> Bool
+allVerifies = undefined
+
+anyVerifies :: (Int -> Bool) -> [Int] -> Bool
+anyVerifies = undefined
+
+listToInt :: [Integer]-> Integer
+listToInt = undefined
+
+rmChar :: Char -> String -> String
+rmChar = undefined
+
+rmCharsRec :: String -> String -> String
+rmCharsRec = undefined
+
+rmCharsFold :: String -> String -> String
+rmCharsFold = undefined
+    
+myUnzip :: [(a, b)] -> ([a], [b])
+myUnzip = undefined
