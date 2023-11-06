b420bd01a6c6826cd6139b5c84847a24ffd4a054
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-06 15:59:10 +0200

    copy lab template

---
+++ lab/lab6.hs
@@ -0,0 +1,92 @@
+data Fruct
+  = Mar String Bool
+  | Portocala String Int
+
+ionatanFaraVierme :: Fruct
+ionatanFaraVierme = Mar "Ionatan" False
+
+goldenCuVierme :: Fruct
+goldenCuVierme = Mar "Golden Delicious" True
+
+portocalaSicilia10 :: Fruct
+portocalaSicilia10 = Portocala "Sanguinello" 10
+
+listaFructe :: [Fruct]
+listaFructe = [Mar "Ionatan" False,
+                Portocala "Sanguinello" 10,
+                Portocala "Valencia" 22,
+                Mar "Golden Delicious" True,
+                Portocala "Sanguinello" 15,
+                Portocala "Moro" 12,
+                Portocala "Tarocco" 3,
+                Portocala "Moro" 12,
+                Portocala "Valencia" 2,
+                Mar "Golden Delicious" False,
+                Mar "Golden" False,
+                Mar "Golden" True]
+
+ePortocalaDeSicilia :: Fruct -> Bool
+ePortocalaDeSicilia = undefined
+
+test_ePortocalaDeSicilia1 :: Bool
+test_ePortocalaDeSicilia1 =
+    ePortocalaDeSicilia (Portocala "Moro" 12) == True
+    
+test_ePortocalaDeSicilia2 :: Bool
+test_ePortocalaDeSicilia2 =
+    ePortocalaDeSicilia (Mar "Ionatan" True) == False
+
+nrFeliiSicilia :: [Fruct] -> Int
+nrFeliiSicilia = undefined
+
+test_nrFeliiSicilia :: Bool
+test_nrFeliiSicilia = nrFeliiSicilia listaFructe == 52
+
+nrMereViermi :: [Fruct] -> Int
+nrMereViermi = undefined
+
+test_nrMereViermi :: Bool
+test_nrMereViermi = nrMereViermi listaFructe == 2
+
+type NumeA = String
+type Rasa = String
+data Animal = Pisica NumeA | Caine NumeA Rasa
+    deriving Show
+
+vorbeste :: Animal -> String
+vorbeste = undefined
+
+rasa :: Animal -> Maybe String
+rasa = undefined
+
+data Linie = L [Int]
+   deriving Show
+data Matrice = M [Linie]
+   deriving Show
+
+verifica :: Matrice -> Int -> Bool
+verifica = undefined
+
+test_verif1 :: Bool
+test_verif1 = verifica (M[L[1,2,3], L[4,5], L[2,3,6,8], L[8,5,3]]) 10 == False
+
+test_verif2 :: Bool
+test_verif2 = verifica (M[L[2,20,3], L[4,21], L[2,3,6,8,6], L[8,5,3,9]]) 25 == True
+
+doarPozN :: Matrice -> Int -> Bool
+doarPozN = undefined
+
+testPoz1 :: Bool
+testPoz1 = doarPozN (M [L[1,2,3], L[4,5], L[2,3,6,8], L[8,5,3]]) 3 == True
+
+testPoz2 :: Bool
+testPoz2 = doarPozN (M [L[1,2,-3], L[4,5], L[2,3,6,8], L[8,5,3]]) 3 == False
+
+corect :: Matrice -> Bool
+corect = undefined
+
+testcorect1 :: Bool
+testcorect1 = corect (M[L[1,2,3], L[4,5], L[2,3,6,8], L[8,5,3]]) == False
+
+testcorect2 :: Bool
+testcorect2 = corect (M[L[1,2,3], L[4,5,8], L[3,6,8], L[8,5,3]]) == True
