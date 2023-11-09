b4a1ec30aa3becc84de6ba18c20a52350fd9637a
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-09 12:54:36 +0200

    Either fns

--- curs/curs6.hs
+++ curs/curs6.hs
@@ -3,3 +3,17 @@ data ForeverList a = ForeverCons a (ForeverList a)
 
 foreverList :: Int -> ForeverList Int
 foreverList nr = let x = ForeverCons nr x in x
+
+addints :: [Either Int String] -> Int
+addints = foldr addints' 0
+    where
+        addints' :: Either Int String -> Int -> Int
+        addints' (Left x) y = x + y
+        addints' (Right _) y = y
+
+addstrs :: [Either Int String] -> String
+addstrs = foldr addstrs' ""
+    where
+        addstrs' :: Either Int String -> String -> String
+        addstrs' (Left _) y = y
+        addstrs' (Right x) y = x ++ y
