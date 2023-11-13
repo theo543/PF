1d8a286ab8599dd7bdf92dd5d0477625176d0ce2
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-13 14:01:12 +0200

    Ad lab 7 drive files

---
+++ lab/lab7.hs
@@ -0,0 +1,81 @@
+data Expr = Const Int -- integer constant
+          | Expr :+: Expr -- addition
+          | Expr :*: Expr -- multiplication
+           deriving Eq
+
+data Operation = Add | Mult deriving (Eq, Show)
+
+data Tree = Lf Int -- leaf
+          | Node Operation Tree Tree -- branch
+           deriving (Eq, Show)
+
+instance Show Expr where
+  show (Const x) = show x
+  show (e1 :+: e2) = "(" ++ show e1 ++ " + "++ show e2 ++ ")"
+  show (e1 :*: e2) = "(" ++ show e1 ++ " * "++ show e2 ++ ")"
+
+evalExp :: Expr -> Int
+evalExp = undefined
+
+exp1 = (Const 2 :*: Const 3) :+: (Const 0 :*: Const 5)
+exp2 = Const 2 :*: (Const 3 :+: Const 4)
+exp3 = Const 4 :+: (Const 3 :*: Const 3)
+exp4 = ((Const 1 :*: Const 2) :*: (Const 3 :+: Const 1)) :*: Const 2
+test11 = evalExp exp1 == 6
+test12 = evalExp exp2 == 14
+test13 = evalExp exp3 == 13
+test14 = evalExp exp4 == 16
+
+evalArb :: Tree -> Int
+evalArb = undefined
+
+
+arb1 = Node Add (Node Mult (Lf 2) (Lf 3)) (Node Mult (Lf 0) (Lf 5))
+arb2 = Node Mult (Lf 2) (Node Add (Lf 3) (Lf 4))
+arb3 = Node Add (Lf 4) (Node Mult (Lf 3) (Lf 3))
+arb4 = Node Mult (Node Mult (Node Mult (Lf 1) (Lf 2)) (Node Add (Lf 3) (Lf 1))) (Lf 2)
+
+test21 = evalArb arb1 == 6
+test22 = evalArb arb2 == 14
+test23 = evalArb arb3 == 13
+test24 = evalArb arb4 == 16
+
+
+expToArb :: Expr -> Tree
+expToArb = undefined
+
+
+data IntSearchTree value
+  = Empty
+  | BNode
+      (IntSearchTree value)     -- elemente cu cheia mai mica
+      Int                       -- cheia elementului
+      (Maybe value)             -- valoarea elementului
+      (IntSearchTree value)     -- elemente cu cheia mai mare
+
+lookup :: Int -> IntSearchTree value -> Maybe value
+lookup = undefined
+
+keys ::  IntSearchTree value -> [Int]
+keys = undefined
+
+values :: IntSearchTree value -> [value]
+values = undefined
+
+insert :: Int -> value -> IntSearchTree value -> IntSearchTree value
+insert = undefined
+
+delete :: Int -> IntSearchTree value -> IntSearchTree value
+delete = undefined
+
+toList :: IntSearchTree value -> [(Int, value)]
+toList = undefined
+
+fromList :: [(Int,value)] -> IntSearchTree value
+fromList = undefined
+
+printTree :: IntSearchTree value -> String
+printTree = undefined
+
+-- balance :: IntSearchTree value -> IntSearchTree value
+-- balance = undefined
